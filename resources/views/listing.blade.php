da6836b170dfdbad4936041f90da3f4d465b58a9
fadwa2022 <fadwacherqui@gmail.com>
2023-02-15 09:06:36 +0100

    Component

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,8 +7,8 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
-    <div
+    <x-card >
+        <div
         class="flex flex-col items-center justify-center text-center"
     >
         <img
@@ -19,28 +19,8 @@
 
         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
         <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-        <ul class="flex">
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Laravel</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">API</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Backend</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Vue</a>
-            </li>
-        </ul>
+        <x-listing-card :listing="$listing" />
+
         <div class="text-lg my-4">
             <i class="fa-solid fa-location-dot"></i>{{$listing->location}} </div>
         <div class="border border-gray-200 w-full mb-6"></div>
@@ -70,6 +50,6 @@
             </div>
         </div>
     </div>
-</div>
-</div>
+</x-card>
+    </div>
 @endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -20,6 +20,7 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
 @foreach($listings as $listing)
 {{-- pour manipuler component --}}
 <x-listing-card :listing="$listing" />
+
 @endforeach
 
 @else
